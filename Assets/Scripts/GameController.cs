2e68e026a42f38a2e74639fd0dca244b3d12c4d8
Ajil Pappachan <ajilpappachan@gmail.com>
2021-02-10 17:08:03 +0530

    Implimented Flood Fill Mechanic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,6 +10,7 @@ public class GameController : MonoBehaviour
     [SerializeField] int width = 18;
     [SerializeField] int height = 32;
     Dictionary<GridIndex, GridCell> gridMap = new Dictionary<GridIndex, GridCell>();
+    List<GridIndex> activeCells = new List<GridIndex>();
 
     [Header("Character Setup")]
     public float BASE_SPEED;
@@ -53,6 +54,7 @@ public class GameController : MonoBehaviour
             {
                 gridMap[index].setActive(true);
                 gridMap[index].setResolved(true);
+                gridMap[index].isBoundary = true;
                 continue;
             }
 
@@ -73,6 +75,109 @@ public class GameController : MonoBehaviour
     public void setCellActive(GridIndex index)
     {
         gridMap[index].setActive(true);
+        activeCells.Add(index);
+        List<GridIndex> neighbours = getNeighbourCells(index);
+        if(neighbours.Count >= 2)
+            floodFill();
+    }
+
+    List<GridIndex> getNeighbourCells(GridIndex index)
+    {
+        List<GridIndex> neighbours = new List<GridIndex>();
+        if (gridMap[new GridIndex(index.x + 1, index.y)].isActive)  neighbours.Add(new GridIndex(index.x + 1, index.y));
+        if (gridMap[new GridIndex(index.x - 1, index.y)].isActive)  neighbours.Add(new GridIndex(index.x - 1, index.y));
+        if (gridMap[new GridIndex(index.x, index.y + 1)].isActive)  neighbours.Add(new GridIndex(index.x, index.y + 1));
+        if (gridMap[new GridIndex(index.x, index.y - 1)].isActive)  neighbours.Add(new GridIndex(index.x, index.y - 1));
+        return neighbours;
+    }
+
+    bool floodFill()
+    {
+        bool didFlood = false;
+        foreach(GridIndex index in gridMap.Keys)
+        {
+            if(!gridMap[index].isActive)
+            {
+                List<GridIndex> activeBounds = new List<GridIndex>();
+                List<GridIndex> LeftCells = new List<GridIndex>();
+                List<GridIndex> RightCells = new List<GridIndex>();
+                List<GridIndex> UpCells = new List<GridIndex>();
+                List<GridIndex> DownCells = new List<GridIndex>();
+                foreach (GridIndex idx in gridMap.Keys)
+                {
+                    if(idx.x == index.x)
+                    {
+                        if(idx.y < index.y)
+                        {
+                            DownCells.Add(idx);
+                        }
+                        if(idx.y > index.y)
+                        {
+                            UpCells.Add(idx);
+                        }
+                    }
+                    if (idx.y == index.y)
+                    {
+                        if (idx.x < index.x)
+                        {
+                            LeftCells.Add(idx);
+                        }
+                        if (idx.x > index.x)
+                        {
+                            RightCells.Add(idx);
+                        }
+                    }
+                }
+                foreach(GridIndex i in LeftCells)
+                {
+                    if (gridMap[i].isActive && !gridMap[i].isBoundary)
+                    {
+                        activeBounds.Add(i);
+                        break;
+                    }
+                }
+                foreach (GridIndex i in RightCells)
+                {
+                    if (gridMap[i].isActive && !gridMap[i].isBoundary)
+                    {
+                        activeBounds.Add(i);
+                        break;
+                    }
+                }
+                foreach (GridIndex i in UpCells)
+                {
+                    if (gridMap[i].isActive && !gridMap[i].isBoundary)
+                    {
+                        activeBounds.Add(i);
+                        break;
+                    }
+                }
+                foreach (GridIndex i in DownCells)
+                {
+                    if (gridMap[i].isActive && !gridMap[i].isBoundary)
+                    {
+                        activeBounds.Add(i);
+                        break;
+                    }
+                }
+                if (activeBounds.Count > 2)
+                {
+                    gridMap[index].setActive(true);
+                    gridMap[index].setResolved(true);
+                    activeCells.Add(index);
+                    didFlood = true;
+                }
+            }
+        }
+        if(didFlood)
+        {
+            foreach (GridIndex idx in activeCells)
+            {
+                gridMap[idx].setResolved(true);
+            }
+            activeCells.Clear();
+        }
+        return didFlood;
     }
 
     void pacMovement()
